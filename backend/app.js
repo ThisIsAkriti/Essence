ea00366fafb2017a1e90330439223c5e95d55932
akriti <akritikohli05@gmail.com>
2025-02-15 16:27:06 +0530

    build the application and it is ready to deploy

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from "path";
 import {connectDB} from './config/db.js'
 import productRoutes from "./routes/product.route.js";
 import cors from'cors';
@@ -13,9 +14,19 @@ app.use(cors({
 }));
 
 const PORT = process.env.PORT || 5000;
+
+const _dirname = path.resolve();
 app.use(express.json());// allows us to accept JSON data in req.body
 
-app.use("/api/products" , productRoutes)
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    });
+};
 app.listen(PORT, () => {
     connectDB()
     console.log("App started at http://localhost:"+PORT);

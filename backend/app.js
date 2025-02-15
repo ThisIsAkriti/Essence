7698bb4e987bba2a8648def20bc3230a9841fc44
akriti <akritikohli05@gmail.com>
2025-02-15 17:16:16 +0530

    commit2

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from "path";
 import {connectDB} from './config/db.js'
 import productRoutes from "./routes/product.route.js";
 import cors from'cors';
@@ -10,24 +9,16 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173' , "https://essence-khaki.vercel.app/"],
     credentials: true
 }));
 
 const PORT = process.env.PORT || 5000;
 
-const _dirname = path.resolve();
 app.use(express.json());// allows us to accept JSON data in req.body
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(_dirname, "/frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-    });
-};
 app.listen(PORT, () => {
     connectDB()
     console.log("App started at http://localhost:"+PORT);

1122b7c24a6310668ff4021744fa63035e62d22e
akriti <akritikohli05@gmail.com>
2025-02-17 18:24:26 +0530

    input boxed width fixed

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -68,9 +68,9 @@ const CreatePage = () => {
 
                 <div className="flex-row space-y-4 px-10">
                 
-                    <div className="dark:bg-gray-800 bg-gray-200 p-4 px-6 rounded-lg text-sm xl:text-lg font-semibold "><input className="outline-none" type="text" placeholder="Name" name="name" value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} /></div>
-                    <div className="dark:bg-gray-800 bg-gray-200 p-4 px-6 rounded-lg text-sm xl:text-lg font-semibold "><input className="outline-none" type="text" placeholder="Price" name="price" value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} /></div>
-                    <div className="dark:bg-gray-800 bg-gray-200 p-4 px-6 rounded-lg text-sm xl:text-lg font-semibold "><input className="outline-none" placeholder="Image" name="image" value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })} /></div>
+                    <div className="dark:bg-gray-800 bg-gray-200 p-4 px-6 rounded-lg text-sm xl:text-lg font-semibold "><input className="outline-none w-full" type="text" placeholder="Name" name="name" value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} /></div>
+                    <div className="dark:bg-gray-800 bg-gray-200 p-4 px-6 rounded-lg text-sm xl:text-lg font-semibold "><input className="outline-none w-full" type="number" placeholder="Price" name="price" value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} /></div>
+                    <div className="dark:bg-gray-800 bg-gray-200 p-4 px-6 rounded-lg text-sm xl:text-lg font-semibold "><input className="outline-none w-full" placeholder="Paste image url in .jpg format only" name="image" value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })} /></div>
                     <button onClick={handleAddProduct} className="bg-green-600 w-full rounded-lg text-md font-semibold py-3 cursor-pointer text-white mt-4 shadow-md active:opacity-70 ">Add Product</button>
                 
                 </div>
